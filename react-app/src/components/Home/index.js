87a3a7127b8942333a34471ba0ba09e2e72b7f77
Joe <Joe.zhang229@gmail.com>
2021-08-08 15:39:13 -0700

    login form styled

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -10,30 +10,35 @@ export default function Home() {
 		<div className='home-container'>
 			<div className='title-container'>
 				<motion.h1
-					initial={{ opacity: 0, y: 0 }}
-					animate={{ opacity: 0.8, y: 150 }}
+					initial={{ opacity: 0, y: 80 }}
+					animate={{ opacity: 0.8, y: 230 }}
 					transition={{ delay: 0.25, duration: 1 }}
 				>
 					Welcome
 				</motion.h1>
 				<motion.h1
-					initial={{ opacity: 0, y: 0 }}
-					animate={{ opacity: 0.8, y: 150 }}
+					initial={{ opacity: 0, y: 80 }}
+					animate={{ opacity: 0.8, y: 230 }}
 					transition={{ delay: 0.5, duration: 1 }}
 				>
 					To
 				</motion.h1>
 				<motion.h1
-					initial={{ opacity: 0, y: 0 }}
-					animate={{ opacity: 0.8, y: 150 }}
+					initial={{ opacity: 0, y: 80 }}
+					animate={{ opacity: 0.8, y: 230 }}
 					transition={{ delay: 0.75, duration: 1 }}
 				>
 					BrewOver
 				</motion.h1>
 			</div>
-			<div className='login-container'>
+			<motion.div
+				className='login-container'
+				initial={{ opacity: 0 }}
+				animate={{ opacity: 0.8 }}
+				transition={{ delay: 1, duration: 1 }}
+			>
 				<LoginForm />
-			</div>
+			</motion.div>
 			<div className='video-container'>
 				<video autoplay='' loop muted>
 					<source src={homeVideo} type='video/mp4'></source>
